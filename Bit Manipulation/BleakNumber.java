f1a2f2dcbc4620fc3adae461a486eb935a622990
NAVDEEP SINGH <navdeep.singh@publicissapient.com>
2022-01-16 19:57:17 +0530

    BleakNumber

--- Bit Manipulation/BleakNumber.java
+++ Bit Manipulation/BleakNumber.java
@@ -7,8 +7,8 @@ public class BleakNumber {
 
     // if input is 7 then before 7 in a number max set bit can be 3
     // so in x + countSetBit(x) => max countSetBit(x) can be 3
-    // so the answer will lie in the range to which we can add 3 and make 7 i.e. 4,
-    // 5, 6
+    // so the answer will lie in the range to which we can add 3 and make 7 i.e. 4, 5, 6
+    // max countSetBit for a number can be calculated by taking its log
     public static void solution(int n) {
 
         for (int i = n - 1; i >= n - ceilLog(n); i--) {
